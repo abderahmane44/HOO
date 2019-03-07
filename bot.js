02fc6855dfe3227a1918e8ab36431f2b1be10fce
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-07 09:11:44 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,17 +55,29 @@ client.on('message', message => {
 	}
 });
 
-
-const gamestats = [`Oméega Bot ,`,`SooN`,`+Help`,`in toop`]
-var index = 0
-var timer = 10 // الوقت بالثواني لتغير الستريمنق
-client.on("ready", ()=> {
-        setInterval(function(){
-        client.user.setGame(`${gamestats[index]}`,'https://www.twitch.tv/ACMBOT') 
-        index++
-            if( index >= gamestats.length) index = 0 ;
-        }, timer*1000);
-
+client.on('message', message => {
+if(message.content.startsWith(prefix +"server")){
+  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** :negative_squared_cross_mark: `)
+if(!message.channel.guild) return message.reply(' ');
+const millis = new Date().getTime() - message.guild.createdAt.getTime();
+const now = new Date();
+dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
+const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
+const days = millis / 1000 / 60 / 60 / 24;
+let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
+var embed  = new Discord.RichEmbed()
+.setAuthor(message.guild.name, message.guild.iconURL)
+.addField("**?? Server ID:**", message.guild.id,true)
+.addField("**?? Created On**", message.guild.createdAt.toLocaleString(),true)
+.addField("**?? Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
+.addField("?? Members ",`[${message.guild.memberCount}]`,true)
+.addField('**?? Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
+.addField("**?? Others **" , message.guild.region,true)
+.addField("** ?? Roles **",`**[${message.guild.roles.size}]** Role `,true)
+.setColor('#000000')
+message.channel.sendEmbed(embed)
+ 
+}
 });
 
 client.on('message', message => {  
