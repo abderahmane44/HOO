25c71ffc742c86d42b6aca2f64d4ad74b7945719
abderahmane44 <46277909+abderahmane44@users.noreply.github.com>
2019-03-07 09:16:14 -1000

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,30 +55,7 @@ client.on('message', message => {
 	}
 });
 
-client.on('message', message => {
-if(message.content.startsWith(prefix +"server")){
-  if(!message.guild.member(message.author).hasPermission("ADMINISTRATOR")) return message.reply(`**هذه الخاصية للادارة فقط** :negative_squared_cross_mark: `)
-if(!message.channel.guild) return message.reply(' ');
-const millis = new Date().getTime() - message.guild.createdAt.getTime();
-const now = new Date();
-dateFormat(now, 'dddd, mmmm dS, yyyy, h:MM:ss TT');
-const verificationLevels = ['None', 'Low', 'Medium', 'Insane', 'Extreme'];
-const days = millis / 1000 / 60 / 60 / 24;
-let roles = client.guilds.get(message.guild.id).roles.map(r => r.name);
-var embed  = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-.addField("**?? Server ID:**", message.guild.id,true)
-.addField("**?? Created On**", message.guild.createdAt.toLocaleString(),true)
-.addField("**?? Owned by**",`${message.guild.owner.user.username}#${message.guild.owner.user.discriminator}`)
-.addField("?? Members ",`[${message.guild.memberCount}]`,true)
-.addField('**?? Channels **',`**${message.guild.channels.filter(m => m.type === 'text').size}**` + ' text | Voice  '+ `**${message.guild.channels.filter(m => m.type === 'voice').size}** `,true)
-.addField("**?? Others **" , message.guild.region,true)
-.addField("** ?? Roles **",`**[${message.guild.roles.size}]** Role `,true)
-.setColor('#000000')
-message.channel.sendEmbed(embed)
- 
-}
-});
+
 
 client.on('message', message => {  
             if(!message.channel.guild) return; 
